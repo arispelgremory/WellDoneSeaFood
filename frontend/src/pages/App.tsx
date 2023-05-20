c6cc10d2f585cf1711fd3a27dde94f45babe98d9
gremoryyx <arispelgremory@gmail.com>
2023-05-20 17:38:06 +0800

    setup tailwindcss for react use

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../utilities/css/App.css';
+
+function App() {
+  return (
+    <div className="text-2xl">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
